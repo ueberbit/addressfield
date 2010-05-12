2595680e7736ab3a9bbe5179d55faec9d0c56afc
Damien Tournoud <damien@tournoud.net>
2010-05-12 16:33:31 +0000

    Initial commit of the addressfield module.

---
+++ theme/addressfield-formatter--US.tpl.php
@@ -0,0 +1,17 @@
+<div class="adr">
+<p class="street-address">
+<?php print implode('<br />', array_filter(array(
+    $address['thoroughfare'],
+    $address['premise'],
+    $address['sub_premise'],
+    $address['dependent_locality'],
+  )));
+?>
+</p>
+<p>
+<span class="locality"><?php print $address['locality'] ?></span>
+<span class="administrative_area"><?php print $address['administrative_area'] ?></span>
+<span class="postal-code"><?php print $address['postal_code'] ?></span>
+</p>
+<p><span class="country"><?php print $address['country'] ?></span></p>
+</div>
