b221f41345441752e71881e469d1f87cf3e959b1
Ryan Szrama <rszrama@49344.no-reply.drupal.org>
2010-11-21 06:09:17 +0000

    Add stub comments, name support, and HTML indentation to the address formatting templates.

--- theme/addressfield-formatter--BR.tpl.php
+++ theme/addressfield-formatter--BR.tpl.php
@@ -1,16 +1,29 @@
-<div class="adr">
-<p class="street-address">
-<?php print implode('<br />', array_filter(array(
-    $address['thoroughfare'] . ', ' . $address['premise'],
-    $address['sub_premise'],
-    $address['dependent_locality'],
-  )));
+<?php
+// $Id$
+
+/**
+ * @file
+ * TODO: Describe what this template is here for.
+ */
 ?>
-</p>
-<p>
-<span class="locality"><?php print $address['locality'] ?></span>
-<span class="administrative_area"><?php print $address['administrative_area'] ?></span>
-<span class="postal-code"><?php print $address['postal_code'] ?></span>
-</p>
-<p><span class="country"><?php print $address['country'] ?></span></p>
+<div class="xnal address-br">
+  <div class="street-address">
+    <?php
+    print implode('<br />', array_filter(array(
+      !empty($address['name_line']) ? $address['name_line'] : $address['first_name'] . ' ' . $address['last_name'],
+      $address['organisation_name'],
+      $address['thoroughfare'] . ', ' . $address['premise'],
+      $address['sub_premise'],
+      $address['dependent_locality'],
+    )));
+    ?>
+  </div>
+  <div>
+    <span class="locality"><?php print $address['locality'] ?></span>
+    <span class="administrative_area"><?php print $address['administrative_area'] ?></span>
+    <span class="postal-code"><?php print $address['postal_code'] ?></span>
+  </div>
+  <div>
+    <span class="country"><?php print $address['country'] ?></span>
+  </div>
 </div>
--- theme/addressfield-formatter.tpl.php
+++ theme/addressfield-formatter.tpl.php
@@ -1,14 +1,31 @@
-<div class="adr">
-<p class="street-address">
-<?php print implode('<br />', array_filter(array(
-    $address['thoroughfare'],
-    $address['premise'],
-    $address['sub_premise'],
-    $address['dependent_locality'],
-  )));
+<?php
+// $Id$
+
+/**
+ * @file
+ * TODO: Describe what this template is here for.
+ */
 ?>
-</p>
-<p><span class="postal-code"><?php print $address['postal_code'] ?></span> <span class="locality"><?php print $address['locality'] ?></span></p>
-<p><span class="administrative_area"><?php print $address['administrative_area'] ?></span></p>
-<p><span class="country"><?php print $address['country'] ?></span></p>
+<div class="xnal address-default">
+  <div class="street-address">
+  <?php
+    print implode('<br />', array_filter(array(
+      !empty($address['name_line']) ? $address['name_line'] : $address['first_name'] . ' ' . $address['last_name'],
+      $address['organisation_name'],
+      $address['thoroughfare'],
+      $address['premise'],
+      $address['sub_premise'],
+      $address['dependent_locality'],
+    )));
+    ?>
+  </div>
+  <div>
+    <span class="postal-code"><?php print $address['postal_code'] ?></span> <span class="locality"><?php print $address['locality'] ?></span>
+  </div>
+  <div>
+    <span class="administrative_area"><?php print $address['administrative_area'] ?></span>
+  </div>
+  <div>
+    <span class="country"><?php print $address['country'] ?></span>
+  </div>
 </div>
