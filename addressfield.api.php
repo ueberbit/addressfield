94dff7ad60f23288aa7736187fe4cc829b9c899b
rszrama <ryan.szrama@gmail.com>
2013-05-01 21:57:38 -0400

    Issue #1406250 by rszrama, jeff.maes: add a hook_addressfield_standard_widget_refresh_alter() to allow the AJAX commands array returned by the refresh callback to be altered.

--- addressfield.api.php
+++ addressfield.api.php
@@ -25,5 +25,23 @@
  * @ingroup addressfield_format
  */
 function CALLBACK_addressfield_format_callback(&$format, $address, $context = array()) {
-  
+  // No example.
+}
+
+/**
+ * Allows modules to add arbitrary AJAX commands to the array returned from the
+ * standard address field widget refresh.
+ *
+ * @param &$commands
+ *   The array of AJAX commands used to refresh the address field widget.
+ * @param $form
+ *   The rebuilt form array.
+ * @param $form_state
+ *   The form state array from the form.
+ *
+ * @see addressfield_standard_widget_refresh()
+ */
+function hook_addressfield_standard_widget_refresh_alter(&$commands, $form, $form_state) {
+  // Display an alert message.
+  $commands[] = ajax_command_alert(t('The address field widget has been updated.'));
 }
