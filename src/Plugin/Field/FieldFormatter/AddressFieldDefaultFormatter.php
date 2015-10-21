70a8d73f4391541b2de46eda209a964b45f81d1e
Florian Weber <florian.weber@ueberbit.de>
2015-10-21 11:17:13 +0200

    Fix AddressFieldDefaultFormatter::viewElements (https://www.drupal.org/node/2575729)

--- src/Plugin/Field/FieldFormatter/AddressFieldDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/AddressFieldDefaultFormatter.php
@@ -100,7 +100,7 @@ class AddressFieldDefaultFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items) {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = array();
 
     foreach ($items as $delta => $item) {
